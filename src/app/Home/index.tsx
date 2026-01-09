8826bed608217af06b675df4edfed9a36ec9c9dd
Bruno Dias <brunoipb1@gmail.com>
2026-01-09 15:45:01 -0300

    adicionando assets

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View, Image } from "react-native"
+import { styles } from "./styles"
+
+export function Home(){
+  return(
+    <View style={styles.container}>
+      <Image source={require("@/assets/logo.png")} style={styles.logo}></Image>
+    </View>
+  )
+}
+

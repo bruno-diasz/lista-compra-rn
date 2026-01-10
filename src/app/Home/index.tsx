cfd31d6ab516ded3155b8b768ec99f4275048b95
Bruno Dias <brunoipb1@gmail.com>
2026-01-09 22:03:00 -0300

    adicionando primeiros componentes

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,10 +1,16 @@
 import { View, Image } from "react-native"
 import { styles } from "./styles"
 
+import { Button } from "@/components/Button"
+import { Input } from "@/components/Input"
+
 export function Home(){
   return(
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo}></Image>
+      <Input />
+      <Button title="Entrar" onPress={() => console.log("Entrou")}/>
+      
     </View>
   )
 }
